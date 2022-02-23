d107c23ebaeb95895494359bae1b8867f07204d9
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-23 14:18:42 +0000

    added a header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#5b6467",
+        backgroundImage: "linear-gradient(315deg, #5b6467 0%, #8b939a 74%)",
+      }}
+    >
+      <Typography variant="h2">POKE-DECKZ</Typography>
+    </Box>
+  );
 };
